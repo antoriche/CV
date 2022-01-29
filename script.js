c0f30aac49495211105633c7e0a5400ea1669878
Antonin <antoriche.ar@gmail.com>
2022-01-29 23:22:17 +0100

    refactor skills

--- script.js
+++ script.js
@@ -1,43 +1,29 @@
-$(function(){
-	var html = "";
-	var i = 0;
-	html+= "<table style='width: 100%;table-layout: fixed;'><tr>";
-	$(".skill").each(function(){
-		//alert($(this).text());
-		html += "<td>"+$(this).text()+"</td>";
-		i++;
-		if(i%3 == 0)html+="</tr><tr>";
-	});
-	html+="</tr></table>";
-	$("#print_skills").html(html);
+$(function () {
+  $(window).resize(function () {
+    languages_size();
+  });
 
-
-	$( window ).resize(function() {
-	  languages_size();
-	});
-
-	languages_size();
-
+  languages_size();
 });
 
-function languages_size(){
-	if($( window ).width() < 992){ // < md
-		$("#languages_print").show();
-		$("#languages_screen").hide();
-	}else{
-		$("#languages_print").hide();
-		$("#languages_screen").show();
-	}
+function languages_size() {
+  if ($(window).width() < 992) {
+    // < md
+    $("#languages_print").show();
+    $("#languages_screen").hide();
+  } else {
+    $("#languages_print").hide();
+    $("#languages_screen").show();
+  }
 }
 
-
-$(function(){
-	$("#sendButon").on("click",function(){
-		sendMail(null,null,$("#contact_message").value());
-	});
+$(function () {
+  $("#sendButon").on("click", function () {
+    sendMail(null, null, $("#contact_message").value());
+  });
 });
 
-function sendMail(from_mail,from_name, msg){
-	var dest = "antoriche.ar@gmail.com";
-	window.open('mailto:'+dest+'?subject=Contact&body='+msg);
+function sendMail(from_mail, from_name, msg) {
+  var dest = "antoriche.ar@gmail.com";
+  window.open("mailto:" + dest + "?subject=Contact&body=" + msg);
 }
